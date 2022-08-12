5ade27358ce3fa889cc30d1890c885ea92dea002
nathanjessen <nathan.jessen@gmail.com>
2022-08-12 12:20:27 -0500

    AboutMe default story

---
+++ components/AboutMe/AboutMe.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, Story } from '@storybook/react';
+import { AboutMe } from './AboutMe';
+
+export default {
+  title: 'AboutMe',
+  component: AboutMe,
+} as Meta;
+
+const Template: Story = (args) => <AboutMe {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
