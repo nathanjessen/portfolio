c38beced8f92d58d667c3d4ddb924dcafb8a3c27
nathanjessen <nathan.jessen@gmail.com>
2023-04-04 02:16:31 -0500

    fix storybook docs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,10 @@
 module.exports = {
-  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../pages/**/*.stories.@(js|jsx|ts|tsx)',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.mdx',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
@@ -11,6 +16,7 @@ module.exports = {
   },
   staticDirs: ['../public'],
   docs: {
-    docsPage: 'automatic',
+    autodocs: true,
+    defaultName: 'Docs',
   },
 };
