6027d8c5119813bc2ae18d8476a50a2bee367994
Nathan Jessen <nathan.jessen@aplaceformom.com>
2024-11-29 19:52:57 -0600

    fix: restore original Projects page implementation

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,11 @@
 import { Metadata } from 'next';
+import { useMemo } from 'react';
+import BasicGrid from '../../components/BasicGrid';
 import { Container } from '../../components/Layouts/Container';
 import PageHeading from '../../components/PageHeading';
-import Projects from '../../components/Projects';
+import ProjectCard from '../../components/ProjectCard';
+import { ProjectCardBasic } from '../../components/ProjectCard/ProjectCardBasic';
+import { projects } from '../../constants/projects';
 import User from '../../constants/User';
 
 export const metadata: Metadata = {
@@ -14,12 +18,40 @@ export const metadata: Metadata = {
 };
 
 const ProjectsPage = () => {
+  const featuredItems = useMemo(
+    () =>
+      projects.filter(
+        (project) => project.featured && project.published !== false
+      ),
+    []
+  );
+  const archivedItems = useMemo(
+    () =>
+      projects.filter(
+        (project) => project.archived && project.published !== false
+      ),
+    []
+  );
+
   return (
     <>
       <PageHeading>Projects</PageHeading>
-      <Container>
-        <Projects />
-      </Container>
+
+      <BasicGrid
+        title='Projects'
+        subtitle='Recent Work'
+        divider={false}
+        items={featuredItems}
+        render={(item, idx) => <ProjectCard item={item} key={idx} />}
+      />
+
+      <BasicGrid
+        title='Archive'
+        subtitle='Past Projects'
+        divider={false}
+        items={archivedItems}
+        render={(item, idx) => <ProjectCardBasic item={item} key={idx} />}
+      />
     </>
   );
 };
