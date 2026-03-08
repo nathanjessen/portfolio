0e29d211b7fd6f003a0b3b36052eb6844f9e6617
Nathan Jessen <nathan.jessen@aplaceformom.com>
2026-03-08 16:47:17 -0500

    fix broken nextjs image

--- constants/techlist.ts
+++ constants/techlist.ts
@@ -6,46 +6,46 @@ export interface TechItem {
 export const techlist: TechItem[] = [
   {
     name: 'React',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/react/react.png',
   },
   {
     name: 'TypeScript',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/typescript/typescript.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/typescript/typescript.png',
   },
   {
     name: 'Next.js',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/nextjs/nextjs.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/nextjs/nextjs.png',
   },
   {
     name: 'Storybook',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/storybook/storybook.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/storybook/storybook.png',
   },
   {
     name: 'Tailwind',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/tailwind/tailwind.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/tailwind/tailwind.png',
   },
   {
     name: 'JavaScript',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/javascript/javascript.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/javascript/javascript.png',
   },
   {
     name: 'AWS',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/aws/aws.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/aws/aws.png',
   },
   {
     name: 'Git',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/git/git.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/git/git.png',
   },
   {
     name: 'CSS',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/css/css.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/css/css.png',
   },
   {
     name: 'Sass',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/sass/sass.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/sass/sass.png',
   },
   {
     name: 'Eslint',
-    img: 'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/eslint/eslint.png',
+    img: 'https://raw.githubusercontent.com/github/explore/main/topics/eslint/eslint.png',
   },
 ];
